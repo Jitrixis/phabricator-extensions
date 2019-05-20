134b44aee0968d6cda43f2d105fb2d8e26498fe1
Julien GARDET <Jitrixis@users.noreply.github.com>
2019-05-20 16:01:31 +0200

    Update ProjectOpenTasksProfileMenuItem.php

--- src/panel/ProjectOpenTasksProfileMenuItem.php
+++ src/panel/ProjectOpenTasksProfileMenuItem.php
@@ -32,15 +32,15 @@ final class ProjectOpenTasksProfileMenuItem
     return $this->getDefaultName();
   }
 
-  protected function newNavigationMenuItems(
+  protected function newMenuItemViewList(
     PhabricatorProfileMenuItemConfiguration $config) {
 
     $object = $config->getProfileObject();
 
     $href = '/maniphest/?project='.$object->getPHID().'&statuses=open()&group=none&order=newest#R';
 
-    $item = $this->newItem()
-      ->setHref($href)
+    $item = $this->newItemView()
+      ->setURI($href)
       ->setName($this->getDisplayName($config))
       ->setIcon('fa-anchor');
 
